2a2972c9f8c19e723e7873bcbca6fb451186721b
Mathieu Grenier <mathieuloco@gmail.com>
2025-08-28 23:34:10 -0400

    Fix: Added env variables

--- assistant-app/services/jira/constants.ts
+++
@@ -1,3 +0,0 @@
-export const REQUIRED_SCOPES = ["read:jira-work", "read:jira-user"].join("%20");
-export const JIRA_CALLBACK_URL = process.env.JIRA_CALLBACK_URL;
-export const JIRA_CLIENT_ID = process.env.JIRA_CLIENT_ID;
--- assistant-app/services/jira/jira-auth.ts
+++ assistant-app/services/jira/jira-auth.ts
@@ -1,19 +1,12 @@
-import {
-  JIRA_CALLBACK_URL,
-  JIRA_CLIENT_ID,
-  REQUIRED_SCOPES,
-} from "./constants";
+const JIRA_CLIENT_ID = process.env.JIRA_CLIENT_ID;
 
 export function getJiraOAuthURL(sessionId: string) {
-  return `https://auth.atlassian.com/authorize?
-  audience=api.atlassian.com&
-  client_id=${JIRA_CLIENT_ID}&
-  scope=${REQUIRED_SCOPES}&
-  redirect_uri=${JIRA_CALLBACK_URL}&
-  state=${sessionId}&
-  response_type=code&
-  prompt=consent
-`;
+  return `https://auth.atlassian.com/authorize?audience=api.atlassian.com
+  &client_id=${JIRA_CLIENT_ID}
+  &scope=read%3Ajira-work%20read%3Ajira-user
+  &redirect_uri=https%3A%2F%2Finternal-ai-knowledge-assistant.vercel.app%2F
+  &state=${sessionId}
+  &response_type=code&prompt=consent`;
 }
 
 export async function redirectToJiraOAuth() {
